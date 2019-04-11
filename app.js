c0c0f7e6736bc1c584390c47e482012190c136f3
omar osama <omarosama200100@gmail.com>
2019-04-11 15:32:43 +0200

    add location from cammnad line

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const forcast = require("./utils/forcast")
 const geocode = require("./utils/geocode")
 
-
-
-
-
-geocode("ismailia",(err,data)=>{
-  if(err){
-      return console.log(err)
-  }
-
-  forcast(data.latitude, data.longitude, (err, forcastData) => {
-      if(err){
-          return console.log(err)
-      }
-
-      console.log(data.location)
-      console.log(forcastData.summary)
-
-  })
+const address = process.argv[2]
+if (!address) {
+    console.log("please provide an address")
 
    
-})
+}
+else {
+    geocode(address, (err, data) => {
+        if (err) {
+            return console.log(err)
+        }
+
+        forcast(data.latitude, data.longitude, (err, forcastData) => {
+            if (err) {
+                return console.log(err)
+            }
+
+            console.log(data.location)
+            console.log(forcastData.summary)
+
+        })
 
 
+    })
+
+
+}
